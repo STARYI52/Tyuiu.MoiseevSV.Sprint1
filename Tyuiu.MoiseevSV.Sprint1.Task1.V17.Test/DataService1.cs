5bddf2d8b02900a2ac6c655cf53df42c2bf91239
stepan moiseev <stepanmoiseev83@gmail.com>
2025-12-08 05:46:35 +0500

    Task1

---
+++ Tyuiu.MoiseevSV.Sprint1.Task1.V17.Test/DataService1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MoiseevSV.Sprint1.Task1.V17.Lib;
+
+
+namespace Tyuiu.MoiseevSV.Sprint1.Task1.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest1
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 2;
+            var result = ds.Calculate(x, y);
+            Assert.AreEqual(12, result);
+        }
+    }
+}
